c0194ea083b402d24319249b1f96a2121efa54fb
wqs <15775831018@163.com>
2017-09-22 16:09:22 +0800

    远程服务器code 上传

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+/**
+ * Created by wqs on 2017/7/4.
+ */
+var mongoose = require('mongoose');
+
+var config = require('./config.js');
+
+module.exports = function () {
+    var db = mongoose.connect(config.mongodb,{ useMongoClient: true });
+
+    require('../models/user.server.model');
+    return db;
+};
+
